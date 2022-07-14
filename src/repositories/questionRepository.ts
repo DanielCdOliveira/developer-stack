2e58fba3a997b6d163d89e53cb7cbba1fc2bf138
Daniel Costa de Oliveira <danielcoliv98@gmail.com>
2022-07-14 11:35:39 -0300

    fix array answers

--- src/repositories/questionRepository.ts
+++ src/repositories/questionRepository.ts
@@ -19,7 +19,7 @@ export async function getQuestionsById(id) {
         // }
         // }
         include:{
-            Answer :{
+            answers :{
                 select:{
                     answer:true
                 }
